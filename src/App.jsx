e8e434c154428d560a7242b6e4cd1f6de0a09cb3
saadhashmi03 <116589433+saadhashmi03@users.noreply.github.com>
2025-10-30 00:44:27 +0530

    add redux store and authSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/auth"
+import { Footer, Header } from './components'
 
 const App = () => {
-  return (
-    <div>App</div>
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+   
+  return  !loading ? (
+    <div className='min-h-screen flex flex-wrap bg-gray-400'>
+      <div className="w-full block">
+        <Header/>
+        <main>
+         TODO: {/* outlet  */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (
+    <div className="h-[20px] w-[20px] rounded-full border-2 border-t-transparent border-red-500 animate-spin"></div>
+
   )
 }
 
